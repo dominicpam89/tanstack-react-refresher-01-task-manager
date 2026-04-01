3031561b8ea35793847fc9e2f24a715a819ee781
dominicpam89 <dominicpam89@gmail.com>
2026-04-01 12:48:09 +0700

    update RootLayout

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router'
 export default function RootLayoutPage() {
   return (
     <div className="w-full min-w-75 max-w-2xl p-2 sm:p-8 m-auto flex flex-col gap-2">
-      <header>Header</header>
       <main>
         <Outlet />
       </main>
