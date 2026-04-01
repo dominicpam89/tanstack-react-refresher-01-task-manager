78a673e0caefc88e53aaae208a1eb38d6e19f60d
dominicpam89 <dominicpam89@gmail.com>
2026-04-01 12:07:06 +0700

    update lib to use queryClient.ts update main.tsx in accordance with the new queryClient.ts delete queryClientProvider.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from '@/lib/queryClientProvider.tsx'
+import { queryClient } from '@/lib/queryClient.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
